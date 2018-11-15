53a76984b3cf38566522f7ec741b46bdb7f9fa7a
bart.malfait@oz.be <bart.malfait@oz.be>
2018-11-15 10:17:22 +0100

    created context

---
+++ ParkShark.Services/Data/ParkSharkContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkShark.Model.Divisions;
+
+namespace ParkShark.Services.Data
+{
+    public class ParkSharkContext : DbContext
+    {
+        private readonly string connectionString;
+
+        public ParkSharkContext()
+        {
+            connectionString = "Data Source=.\\SQLExpress;Initial Catalog=ParkShark;Integrated Security=True;";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(this.connectionString);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Division>()
+                .ToTable("Divisions")
+                .HasKey("ID");
+
+        }
+
+    }
+}
