0c538b6dfba358c8ad66ce9424719fcf4d63f85e
LucVerhoevenCarglass <luc.verhoeven@carglass.be>
2018-11-15 10:37:32 +0100

    ParkSharkLuc

---
+++ ParkShark.Model/Parkinglots/Parkinglot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkShark.Model.Parkinglots
+{
+    public class Parkinglot
+    {
+        public string Name { get; set; }
+        //public string  { get; set; }
+    }
+}
