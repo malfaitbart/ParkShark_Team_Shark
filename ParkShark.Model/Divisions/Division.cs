33225dc4425f8f32edce34db9302e2d5d0e5830d
bart.malfait@oz.be <bart.malfait@oz.be>
2018-11-15 10:18:14 +0100

    added division class

---
+++ ParkShark.Model/Divisions/Division.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkShark.Model.Divisions
+{
+    public class Division
+    {
+        public int ID { get; private set; }
+        public string Name { get; private set; }
+        public int PersonDirectorId { get; private set; }
+        public int ParentDivisionId { get; private set; }
+
+        public Division(string name, int personDirectorId)
+        {
+            Name = name;
+            PersonDirectorId = personDirectorId;
+        }
+    }
+}
